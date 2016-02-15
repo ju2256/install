210316ee173b339a6c0e486707402b33786bd748
ju2256 <julienquiltu@gmail.com>
2016-02-15 11:31:35 +0100

    Update captive-portal.sh

--- captive-portal/captive-portal.sh
+++ captive-portal/captive-portal.sh
@@ -1,2 +1,7 @@
-cd /
-cd /tmp
+#!/bin/bash
+# script d'installation du portail captif
+[ -d /data/src/captive_portal ] && echo "data Exists" || mkdir /data/src/captive_portal
+cd /data/src/captive_portal
+git clone https://github.com/ju2256/captive_portal.git
+cp -r captive_portal /var/www/captive_portal
+
